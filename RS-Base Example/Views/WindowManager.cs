50642fd44f9c835ec48efe2f15b9abc9a3288734
Björn Ångbäck <bjorn@angback.se>
2020-10-11 20:22:10 +0200

    Need to create a window manager...

---
+++ RS-Base Example/Views/WindowManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RS_Base.Views
+{
+    public class WindowManager
+    {
+        public WindowManager()
+        {
+
+        }
+    }
+}
