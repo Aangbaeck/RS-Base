ee5a15173472ecc2bd32f156262b3901a93e89f2
Björn Ångbäck <bjorn@angback.se>
2021-09-20 10:27:20 +0200

    smågrejer, uppdaterat nugets

--- RS-Base Clean/Views/ViewModelLocator.cs
+++ RS-Base Clean/Views/ViewModelLocator.cs
@@ -1,18 +1,10 @@
 ﻿using System.Globalization;
 using Autofac;
-using GalaSoft.MvvmLight.Ioc;
 using RS_Base.Net.Model;
 using RS_StandardComponents;
 
 namespace RS_Base.Views
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// <para>
-    /// See http://www.mvvmlight.net
-    /// </para>
-    /// </summary>
     public class ViewModelLocator
     {
         private static ViewModelLocator instance = null;
@@ -34,12 +26,12 @@ namespace RS_Base.Views
         }
         static ViewModelLocator()
         {
-                        var builder = new ContainerBuilder();
+            var builder = new ContainerBuilder();
             builder.RegisterType<WindowManager>().SingleInstance();
             builder.RegisterType<SettingsService>().SingleInstance();
             builder.RegisterType<MainVM>().SingleInstance();
             Container = builder.Build();
-            
+
             //Setting language for whole application
             CultureManager.UICulture = new CultureInfo(Container.Resolve<SettingsService>().Settings.Language);
         }
